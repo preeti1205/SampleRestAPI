5a3b71baa8a38326a63488f8f46f4337ce432745
Preeti Bhandari <preeti1205@users.noreply.github.com>
2020-03-23 23:06:45 -0400

    Add request functions

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for
+from flask import Flask,url_for, request, json
 app=Flask(__name__)
 
 @app.route('/')
@@ -13,5 +13,48 @@ def api_articles():
 def api_article(articleid):
     return 'You are reading ' + articleid
 
+@app.route('/hello')
+def api_hello():
+    if 'name' in request.args:
+        return 'Hello' + request.args['name']
+    else:
+        return 'Hello Preeti'
+
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE' ])
+# use "curl -X PUT http://127.0.0.1:5000/echo" to test
+def api_echo():
+	if request.method == 'GET':
+		return "ECHO: GET\n"
+
+	elif request.method == 'POST':
+		return "ECHO: POST\n"
+
+	elif request.method == 'PATCH':
+		return "ECHO: PATCH\n"
+
+	elif request.method == 'PUT':
+		return "ECHO: PUT\n"
+
+	elif request.method == 'DELETE':
+		return "ECHO: DELETE"
+
+@app.route('/message', methods=['POST'])
+def api_message():
+	if request.headers['Content-type'] == 'text/plain':
+		return "Text Message: " + request.data
+
+	elif request.headers['Content-type'] == 'application/json':
+		return "JSON Message: " + json.dumps(request.json)
+
+	elif request.headers['Content-type'] == 'application/octet-stream':
+		f = open('./binary', 'wb')
+		f.write(request.data)
+		f.close()
+		return "Binary message written"
+
+	else:
+		return "415 : Media type not supported"
+
+    
 if __name__=='__main__':
     app.run()
